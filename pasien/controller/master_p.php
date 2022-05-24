712f118babdad4a6862526a8bc4fa20fb688a34b
Rifaldi Setiawan <rifaldisetiawan6@gmail.com>
2022-05-24 21:50:12 +0700

    update

--- pasien/controller/master_p.php
+++ pasien/controller/master_p.php
@@ -61,12 +61,7 @@
                     else{
                         $sql="SELECT * FROM v_laporan  where tanggal_laporan>='".$_GET['start_date']."' and tanggal_laporan<='".$_GET['end_date']."' and id_ibu_hamil='".$_SESSION['id_user']."'";
                     }
-                    $r=mysqli_query($conn,$sql);
-                ?>
-                    <tr>
-                        <td colspan="12" class="fw-bold">DATA LAPORAN</td>
-                    </tr>
-                <?php
+                    $r=mysqli_query($conn,$sql); 
                     while($rs=mysqli_fetch_array($r)){
                 ?>
                         <tr style="border:1px solid #ddd;">
